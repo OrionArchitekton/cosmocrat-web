2583d479f4e068372ae3e1661879a7ca7d3fc56e
OrionArchitekton <dan.mercede@gmail.com>
2026-01-24 01:04:05 -0800

    feat(analytics): add Google Analytics (gtag.js) G-RPJ9C1KYMF

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { siteConfig } from '@/lib/siteConfig';
 import '../styles/globals.css';
 
 const GTM_ID = 'GTM-K3RWK8SD';
+const GA_ID = 'G-RPJ9C1KYMF';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -135,6 +136,24 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','${GTM_ID}');`,
+          }}
+        />
+
+        {/* Google Analytics (gtag.js) */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}');
+            `,
           }}
         />
       </body>
